6ec820d4443eb8aaeaa568bdc6bb93a6b2f325de
LAPTOP-HR8PS1Q0\Admin <weishawtyhao9@gmail.com>
2022-12-02 17:57:24 +0800

    Generate authentication token for front-end

--- User-Service/config/config.js
+++ User-Service/config/config.js
@@ -1,6 +1,7 @@
 const config = {
   DB_LOCAL_URI:
     process.env.DB_LOCAL_URI || "mongodb://localhost:27017/peerPrep",
+  SECRET_TOKEN: process.env.SECRET_TOKEN || "your-very-secretive-key",
 };
 
 module.exports = config;
--- User-Service/models/user.js
+++ User-Service/models/user.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const config = require("../config/config");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -31,7 +34,25 @@ const userSchema = new mongoose.Schema({
       }
     },
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user.id.toString() }, config.SECRET_TOKEN);
+
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+
+  return token;
+};
 
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email });
