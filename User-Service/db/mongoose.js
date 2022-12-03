59cacc2d41af1572209a9a13909017e2ccd5d2b1
LAPTOP-HR8PS1Q0\Admin <weishawtyhao9@gmail.com>
2022-12-03 15:21:58 +0800

    dockerize user service

--- User-Service/db/mongoose.js
+++ User-Service/db/mongoose.js
@@ -5,3 +5,5 @@ const config = require("../config/config");
 mongoose.connect(config.DB_LOCAL_URI, {
   useNewUrlParser: true,
 });
+
+// /Users/Admin/mongodb/bin/mongod.exe --dbpath=/Users/Admin/mongodb-data
