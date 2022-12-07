5ef92e135d1aad6b9aa4c057106a16328ff1036c
LAPTOP-HR8PS1Q0\Admin <weishawtyhao9@gmail.com>
2022-12-07 15:21:15 +0800

    Initialize matching microservice

---
+++ Matching-Service/index.js
@@ -0,0 +1,15 @@
+const dotenv = require("dotenv");
+const express = require("express");
+require("./db/mongoose");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8002;
+
+app.use(express.json());
+// app.use(matchingRouter);
+
+app.listen(port, () => {
+  console.log("Matching Service listening on port", port);
+});
